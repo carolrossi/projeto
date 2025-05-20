14363a1eb812a11c4a4f83ba3d8de018aace2a27
carolrossi <anacarollinirossi@gmail.com>
2025-05-20 11:11:42 -0300

    qrcode

--- src/app/page.js
+++ src/app/page.js
@@ -9,10 +9,11 @@ export default function Portal() {
     cpf: '',
     senha: ''
   });
+  const [showModal, setShowModal] = useState(false);
 
   const handleButtonClick = (formType) => {
     setActiveForm(formType);
-    setFormData({ ra: '', cpf: '', senha: '' }); // Limpa os dados ao trocar de formulário
+    setFormData({ ra: '', cpf: '', senha: '' });
   };
 
   const handleCloseForm = () => {
@@ -49,50 +50,38 @@ export default function Portal() {
         </div>
       </div>
 
-      <div className={`flex flex-col items-center justify-center text-[#1f557b] font-bold text-6xl titulo transition-opacity duration-700 ${activeForm ? 'fade-out' : 'fade-in'} `}>
+      <div className={`flex flex-col items-center justify-center text-[#1f557b] font-bold text-6xl titulo transition-opacity duration-700 ${activeForm ? 'fade-out' : 'fade-in'}`}>
         <h1 className='text-shadow-lg'>Olá, seja bem vindo!</h1>
-        <h4 className='text-2xl'>texto texto texto</h4>
+        <h4 className='text-2xl'>Controle acadêmico de presença de forma prática e segura.</h4>
       </div>
 
-      <div className="flex-1 flex flex-col items-center  justify-center pb-70 "> 
+      <div className="flex-1 flex flex-col items-center justify-center pb-70">
         <div className={`transition-opacity duration-700 ${activeForm ? 'fade-out' : 'fade-in'}`}>
-        <h1 className='m-5 text-[#1f557b] font-bold text-[18px]'>Escolha abaixo um perfil para acessar.</h1>
-      </div>
+          <h1 className='m-5 text-[#1f557b] font-bold text-[18px]'>Escolha abaixo um perfil para acessar.</h1>
+        </div>
+
         {!activeForm ? (
           <div className="flex gap-10">
-            <button 
-              onClick={() => handleButtonClick('aluno')}
-              className="w-40 h-20 bg-white rounded-[10px] cursor-pointer hover:bg-blue-200 transition-all duration-300 shadow-2xl hover:shadow-blue-500/50 hover:-translate-y-1" 
-              type="button"
-            >
-              Aluno
-            </button>
-            <button 
-              onClick={() => handleButtonClick('professor')}
-              className="w-40 h-20 bg-white rounded-[10px] cursor-pointer hover:bg-blue-200 transition-all duration-300 shadow-2xl hover:shadow-blue-500/50 hover:-translate-y-1" 
-              type="button"
-            >
-              Professor
-            </button>
-            <button 
-              onClick={() => handleButtonClick('coordenacao')}
-              className="w-40 h-20 bg-white rounded-[10px] cursor-pointer hover:bg-blue-200 transition-all duration-300 shadow-2xl hover:shadow-blue-500/50 hover:-translate-y-1" 
-              type="button"
-            >
-              Coordenação
-            </button>
+            {['aluno', 'professor', 'coordenacao'].map(type => (
+              <button
+                key={type}
+                onClick={() => handleButtonClick(type)}
+                className="w-40 h-20 bg-white rounded-[10px] cursor-pointer hover:bg-blue-200 transition-all duration-300 shadow-2xl hover:shadow-blue-500/50 hover:-translate-y-1"
+                type="button"
+              >
+                {type.charAt(0).toUpperCase() + type.slice(1)}
+              </button>
+            ))}
           </div>
         ) : (
-         
-          
-          <div className="bg-white p-10 rounded-[20px] shadow-2xl w-full max-w-md slide-in  ">
+          <div className="bg-white p-10 rounded-[20px] shadow-2xl w-full max-w-md slide-in">
             <div className="flex justify-between items-center mb-2">
               <h3 className="text-xl font-bold text-gray-800">
                 {activeForm === 'aluno' && 'Login do Aluno'}
                 {activeForm === 'professor' && 'Login do Professor'}
                 {activeForm === 'coordenacao' && 'Login da Coordenação'}
               </h3>
-              <button 
+              <button
                 onClick={handleCloseForm}
                 className="text-gray-500 hover:text-gray-700"
               >
@@ -152,7 +141,7 @@ export default function Portal() {
               <div>
                 <button
                   type="submit"
-                  className="w-full flex justify-center py-2 px-4 border border-transparent  shadow-sm text-sm font-medium text-white bg-[#1f557b] rounded-[10px] cursor-pointer hover:bg-[#0e3754] transition-all duration-300  "
+                  className="w-full flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium text-white bg-[#1f557b] rounded-[10px] cursor-pointer hover:bg-[#0e3754] transition-all duration-300"
                 >
                   Acessar
                 </button>
@@ -161,7 +150,16 @@ export default function Portal() {
 
             <div className="mt-4 text-center text-sm text-gray-600">
               {activeForm === 'aluno' ? (
-                <p>Problemas com seu R.A.? Entre em contato com a secretaria.</p>
+                <>
+                  <p>Problemas com seu R.A.? Entre em contato com a secretaria.</p>
+                  <hr className="my-4 h-0.5 border-t-0 bg-neutral-100 dark:bg-white/10" />
+                  <p
+                    onClick={() => setShowModal(true)}
+                    className="cursor-pointer hover:text-blue-900 mt-4 text-center text-sm text-blue-600 font-semibold"
+                  >
+                    Entre em outros dispositivos através do QR code!
+                  </p>
+                </>
               ) : (
                 <p>Esqueceu sua senha? Redefina através do e-mail institucional.</p>
               )}
@@ -169,6 +167,30 @@ export default function Portal() {
           </div>
         )}
       </div>
+
+
+      {showModal && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-[rgba(0,0,0,0.5)] backdrop-blur-sm opacity-100 ">
+          <div className="bg-white rounded-lg shadow-md w-full max-w-md p-6 relative transform transition-all duration-500 scale-100 opacity-100">
+       
+            <div className="flex justify-between items-center border-b pb-2 mb-4">
+              <h3 className="text-xl font-semibold text-gray-900 flex items-center justify-center">QR Code</h3>
+              <button
+                onClick={() => setShowModal(false)}
+                className="text-gray-400 hover:text-gray-600"
+              >
+                ✕
+              </button>
+            </div>
+
+            <div className=' flex flex-col items-center justify-center'>
+              <p>Escaneie o QR code no seu telefone/tablet:</p>
+              <img src='./qrcode.jpg' ></img>
+            </div>
+          
+          </div>
+        </div>
+      )}
     </div>
   );
 }
