9d95730a7af923d72ddc00d43482b1b9245ffd7a
carolrossi <anacarollinirossi@gmail.com>
2025-05-21 11:11:37 -0300

    home

--- src/app/components/Header/header.jsx
+++ src/app/components/Header/header.jsx
@@ -2,8 +2,14 @@ import './header.css'
 
 export default function HomeAluno() {
     return (
-      <header className="h-42 bg-blue-950 shadow">
+      <header className="bg-blue-950 shadow">
         
+        <div className='flex flex-col items-center justify-center text-4xl shadow-xl/30 shadow-blue-900 '>
+          <img src='./logo2.png' width={200}  />
+        <h1 className='text-white font-bold '> Seja Bem-vindo ao Portal de frequência do Aluno!</h1>
+        <h4 className='text-white text-2xl mt-2 mb-4'> texto texto texto texto texto texto</h4>
+
+        </div>
       </header>
     );
   }
